46cb60bd3699887302d900fad708dae484a942e8
Larry Mei <91803291+syndromenuts@users.noreply.github.com>
2026-03-11 23:04:01 -0700

    Change total grid size to be constant
    Before, the canvas size was changing based on the number of squares and
    the square size was constant. Now, to follow the project instructions
    correctly, the square size changes while the canvas size stays at 625px.
    
    Additionaly, the parent container would fill when the number of squares
    wasn't perfectly divisible, since there would be a tiny gap on the
    edges of the canvas, and when the mouse hovers over this gap, it would
    make the even trigger for the whole container, so I added a check to
    make sure that only squares get filled.

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-const tempNumSquares = 16;
-const SQUARE_PX_SIZE = 25;
+const tempNumSquares = 17;
 
 const squares = document.querySelector("#squares");
+
+const canvasSize = squares.getBoundingClientRect().width;
+const squareSize = canvasSize / tempNumSquares;
+
 for (let i = 0; i < tempNumSquares ** 2; i++) {
     const square = document.createElement("div")
     square.classList.add("square");
-    square.style.width = SQUARE_PX_SIZE + "px";
-    square.style.height = SQUARE_PX_SIZE + "px";
+    square.style.width = squareSize + "px";
+    square.style.height = squareSize + "px";
     squares.appendChild(square);
 }
 
-squares.style.width = SQUARE_PX_SIZE * tempNumSquares + "px";
-squares.style.height = SQUARE_PX_SIZE * tempNumSquares + "px";
-
 function fillSquare(event) {
-    event.target.classList.add("filled-square");
+    if (event.target.classList.contains("square")) {
+        event.target.classList.add("filled-square");
+    }
 }
 
 squares.addEventListener("mouseover", fillSquare)
