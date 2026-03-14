34d9f5f0ca688dd51322622d427291b3ad441321
Larry Mei <91803291+syndromenuts@users.noreply.github.com>
2026-03-14 00:47:56 -0700

    Add new grid function
    Put the previous code for creating the grid into a function so that
    there isn't duplicate code. Renamed some variables for clarity. Changed
    event lister callbacks to arrow functions for clarity.
    
    Didn't format the button and don't think I will. It works and looks ok.

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
-const tempNumSquares = 17;
-
 const squares = document.querySelector("#squares");
-
 const canvasSize = squares.getBoundingClientRect().width;
-const squareSize = canvasSize / tempNumSquares;
 
-for (let i = 0; i < tempNumSquares ** 2; i++) {
-    const square = document.createElement("div")
-    square.classList.add("square");
-    square.style.width = squareSize + "px";
-    square.style.height = squareSize + "px";
-    squares.appendChild(square);
-}
+function createGrid(sideLength) {
+    const squareSize = canvasSize / sideLength;
 
-function fillSquare(event) {
-    if (event.target.classList.contains("square")) {
-        event.target.classList.add("filled-square");
+    for (let i = 0; i < sideLength ** 2; i++) {
+        const square = document.createElement("div")
+        square.classList.add("square");
+        square.style.width = squareSize + "px";
+        square.style.height = squareSize + "px";
+        squares.appendChild(square);
     }
+
+    squares.addEventListener("mouseover", (event) => {
+        // fill square
+        if (event.target.classList.contains("square")) {
+            event.target.classList.add("filled-square");
+        }
+    })
 }
 
-squares.addEventListener("mouseover", fillSquare)
+const INITIAL_SIDE_LENGTH = 16;
+createGrid(INITIAL_SIDE_LENGTH);
+
+const newButton = document.querySelector("#new");
+newButton.addEventListener("click", (event) => {
+    let input = Number(prompt("Enter a side length for the grid (max 100)"));
+
+    while (!(Number.isInteger(input) && input <= 100 && input >= 1)) {
+        input = Number(prompt("Enter a side length for the grid (max 100)"));
+    }
+
+    squares.replaceChildren();
+
+    // input should be an integer that is less than 100 at this point
+    createGrid(input);
+});
